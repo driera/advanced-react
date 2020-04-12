8f4830d64ab106ef665a40175bb22c93a2fba01b
Dani Riera <daniel.riera@qatium.com>
2020-04-12 23:57:09 +0200

    Add _app custom layout and link pages with Next.js

---
+++ frontend/components/Meta.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=devide-width, initial-scale=1" />
+      <meta charset="utf-8" />
+      <link
+        rel="shortcut icon"
+        href="/static/favicon.ico"
+        type="image/x-icon"
+      />
+      <link rel="stylesheet" href="/static/nprogress.css" />
+      <title>.node Sick Fits!</title>
+    </Head>
+  );
+};
+
+export default Meta;
---
+++ frontend/components/Page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import Meta from "../components/Meta";
+
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Page;
