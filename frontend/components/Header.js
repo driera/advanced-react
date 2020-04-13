2d27a500ef1017ef2108328db53d48a9a4721f65
Dani Riera <daniel.riera@qatium.com>
2020-04-13 18:56:00 +0200

    Move HEader styles to styled component

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,8 +1,8 @@
 import Nav from "./Nav";
 import Link from "next/link";
-import styled from "styled-components";
 import Router from "next/router";
 import NProgress from "nprogress";
+import StyledHeader, { Logo } from "./styles/HeaderStyles";
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -16,54 +16,12 @@ Router.onRouteChangeError = () => {
   NProgress.done();
 };
 
-const Logo = styled.h1`
-  position: relative;
-  z-index: 2;
-  margin-left: 2rem;
-  font-size: 4rem;
-  transform: skew(-7deg);
-
-  a {
-    padding: 0.5rem 1rem;
-    color: ${(props) => props.theme.white};
-    text-transform: uppercase;
-    text-decoration: none;
-    background: ${(props) => props.theme.red};
-  }
-
-  @media (max-width: 1300px) {
-    margin: 0;
-    text-align: center;
-  }
-`;
-
-const StyledHeader = styled.header`
-  .bar {
-    border-bottom: 1px solid ${(props) => props.theme.black};
-    display: grid;
-    grid-template-columns: auto 1fr;
-    justify-content: space-between;
-    align-items: stretch;
-
-    @media (max-width: 1300px) {
-      grid-template-columns: 1fr;
-      justify-content: center;
-    }
-  }
-
-  .sub-bar {
-    display: grid;
-    grid-template-columns: 1fr auto;
-    border-bottom: 1px solid ${(props) => props.theme.lightGrey};
-  }
-`;
-
 const Header = () => (
   <StyledHeader>
     <div className="bar">
       <Logo>
         <Link href="/">
-          <a>.Node Sick Fits</a>
+          <a>.Node Fits</a>
         </Link>
       </Logo>
       <Nav />
